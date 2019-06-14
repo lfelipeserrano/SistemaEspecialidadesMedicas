2e315353ae238dc2182e639a71a852e47eb8e676
lfelipeserrano <felipeserrano@fia.ues.edu.sv>
2019-06-14 16:17:39 -0600

    Modelos modificados

--- CEM/forms.py
+++ CEM/forms.py
@@ -6,7 +6,7 @@ class DoctorForm(forms.ModelForm):
     class Meta:
         model = Doctor
         fields = ('primerNombreDoctor', 'segundoNombreDoctor', 'primerApellidoDoctor', 'segundoApellidoDoctor', 'especialidad', 'sexoDoctor', 'fechaNacimientoDoctor',
-        'telefonoDoctor', 'correoElectronico', 'duiDoctor', 'fotografiaDoctor', )
+        'telefonoDoctor', 'correoElectronico', 'duiDoctor', 'nitDoctor', 'ncfDoctor', 'fotografiaDoctor', )
 
     def __init__(self, *args, **kwargs):
         super(DoctorForm, self).__init__(*args, **kwargs)
@@ -15,10 +15,11 @@ class DoctorForm(forms.ModelForm):
 class PacienteForm(forms.ModelForm):
     class Meta:
         model = Paciente
-        fields = ('idDoctor', 'expediente','primerNombrePaciente', 'segundoNombrePaciente', 'primerApellidoPaciente', 'segundoApellidoPaciente', 'sexoPaciente', 'fechaNacimientoPaciente', 
-        'alturaPaciente', 'pesoPaciente', 'telefonoPaciente', 'fotografiaPaciente', 'institucion', 'alergias', 'antecedentes', )
+        fields = ('doctores', 'expediente','primerNombrePaciente', 'segundoNombrePaciente', 'primerApellidoPaciente', 'segundoApellidoPaciente', 'sexoPaciente', 'fechaNacimientoPaciente', 
+        'alturaPaciente', 'pesoPaciente', 'telefonoPaciente', 'fotografiaPaciente', 'institucion', 'aseguradora', 'alergias', 'lugarProveniencia', 'tabquista', 'etilista',
+        'hipertenso', 'diabetico', 'tuberculosis', 'anemia', 'convulsiones', 'convulsiones', 'cancer', 'lugarCancer', 'tratamientoCancer', 'antecedentes', )
 
-    idDoctor = forms.ModelChoiceField(queryset=Doctor.objects.all())
+    # idDoctor = forms.ModelChoiceField(queryset=Doctor.objects.all())
 
     def __init__(self, *args, **kwargs):
         super(PacienteForm, self).__init__(*args, **kwargs)
--- CEM/models.py
+++ CEM/models.py
@@ -5,38 +5,49 @@ from datetime import date
 class Doctor(models.Model):
     idDoctor = models.AutoField(primary_key=True)
     primerNombreDoctor = models.CharField(max_length=50)
-    segundoNombreDoctor = models.CharField(max_length=50, blank = True)
+    segundoNombreDoctor = models.CharField(max_length=50, blank = True, null=True)
     primerApellidoDoctor = models.CharField(max_length=50)
-    segundoApellidoDoctor = models.CharField(max_length=50, blank = True)
+    segundoApellidoDoctor = models.CharField(max_length=50, blank = True, null=True)
     especialidad = models.CharField(max_length=50)
     sexoDoctor = models.BooleanField()
     fechaNacimientoDoctor = models.DateField()
     telefonoDoctor = models.CharField(max_length=9)
     correoElectronico = models.CharField(max_length=50)
     duiDoctor = models.CharField(max_length = 12)
-    fotografiaDoctor = models.ImageField(upload_to = 'doctores', blank = True)
+    nitDoctor = models.CharField(max_length = 20)
+    ncfDoctor = models.CharField(max_length = 15)
+    fotografiaDoctor = models.ImageField(upload_to = 'doctores', blank = True, null=True)
 
     def __str__(self):
         return self.primerApellidoDoctor
 
 class Paciente(models.Model):
     expediente = models.CharField(max_length = 20, primary_key = True)
-    idDoctor = models.ForeignKey(
-        Doctor,
-        on_delete = models.CASCADE
-    )
+    doctores = models.ManyToManyField(Doctor)
     primerNombrePaciente = models.CharField(max_length=50)
-    segundoNombrePaciente = models.CharField(max_length=50, blank = True)
+    segundoNombrePaciente = models.CharField(max_length=50, blank = True, null = True)
     primerApellidoPaciente = models.CharField(max_length=50)
-    segundoApellidoPaciente = models.CharField(max_length=50, blank = True)
+    segundoApellidoPaciente = models.CharField(max_length=50, blank = True, null = True)
     sexoPaciente = models.BooleanField()
     fechaNacimientoPaciente = models.DateField()
     alturaPaciente = models.IntegerField(blank = True, null=True)
     pesoPaciente = models.FloatField(max_length=5, blank = True, null=True)
-    telefonoPaciente = models.CharField(max_length=9, blank = True)
-    fotografiaPaciente = models.ImageField(upload_to = 'cem/imagenes/pacientes/', blank = True)
-    institucion = models.CharField(max_length = 50, blank = True)
+    telefonoPaciente = models.CharField(max_length=9, blank = True, null = True)
+    fotografiaPaciente = models.ImageField(upload_to = 'cem/imagenes/pacientes/', blank = True, null = True)
+    institucion = models.CharField(max_length = 50, blank = True, null = True)
+    aseguradora = models.CharField(max_length = 50, blank = True, null = True)
     alergias = models.TextField(max_length = 200, blank = True)
+    lugarProveniencia = models.CharField(max_length = 100, blank = True, null = True)
+    tabquista = models.BooleanField()
+    etilista = models.BooleanField()
+    hipertenso = models.BooleanField()
+    diabetico = models.BooleanField()
+    tuberculosis = models.BooleanField()
+    anemia = models.BooleanField()
+    convulsiones = models.BooleanField()
+    cancer = models.BooleanField()
+    lugarCancer = models.CharField(max_length = 50, blank = True, null = True)
+    tratamientoCancer = models.CharField(max_length = 100, blank = True, null = True)
     antecedentes = models.TextField(max_length = 200, blank = True)
 
     def __str__(self):
@@ -61,6 +72,8 @@ class Consulta(models.Model):
     observaciones = models.TextField(max_length=500)
     recetas = models.TextField(max_length=200, blank=True, null= True)
     examenesSolicitados = models.TextField(max_length=200, blank=True, null= True)
+    reporteExamenes = models.TextField(max_length= 200, blank=True, null=True)
+    fechaUltimaRegla = models.DateField(null = True, blank = True)
 
     def __str__(self):
         return self.fechaConsulta.strftime("%d/%m/%Y")
