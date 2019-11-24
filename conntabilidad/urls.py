12e5a6f3ada31af81cb5e613be9b37059bd721b8
lfelipeserrano <felipeserrano@fia.ues.edu.sv>
2019-11-24 00:25:38 -0600

    CRUD de pagos agregado

---
+++ conntabilidad/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from .views import *
+from . import views
+from CEM.views import permisoDenegado
+
+urlpatterns = [
+    path('conta/pago/<int:pk>/eliminar', pagoEliminarFuncion, name='pago_eliminar'),
+    path('conta/pago/<int:pk>/editar', pagoEditar, name='pago_editar'),
+    path('conta/pago/nuevo/', pagoNuevo, name='pago_nuevo'),
+    path('conta/pago/list/', pagos, name='pagos'),
+    path('conta/pago/<int:pk>/', pagoDatos, name='pago_datos')
+]
